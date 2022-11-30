9f303635b741301278c85793adf86f1c556d103f
Hindax <haar162@more2learn.de>
2022-11-30 10:46:35 +0100

    Interfaces

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,16 +1,18 @@
 package com.cc.java;
 
 public class App {
-    
+
     public static void main(String[] args) {
-     
+
+        Bird bird = new Bird();
+
+        output(bird.fly());
+        output(bird.hasFeathers());
+
     }
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
     }
 
-
-
 }
-
